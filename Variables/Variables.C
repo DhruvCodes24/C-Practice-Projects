1275383d68bb6f905fcd2e5a125f7a6123c117d6
Dhruv <dhruvcodes24@gmail.com>
2026-08-19 19:12:17 +0530

    Variables, Comments

---
+++ Variables/Variables.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+// This Code Represents the use of variables in C programming language
+
+/*
+   This is a multi-line comment
+*/
+
+    int number = 25;
+    char star = '*';
+    int age = 22;
+    float pi = 3.14;
+
+    int a= 30;
+    int A = 40;
+
+    int _age = 23;
+
+    int final_price = 2000;
+
+    return 0;
+
+}
